809ed2d86ca7d7cb2d0b48d867f1a8db52c96adc
Kartikeya Mishra <kmishra@umd.edu>
2022-11-19 01:21:43 -0500

    Add lorenz system

---
+++ lorenz_system.m
@@ -0,0 +1,17 @@
+function dX = lorenz_system(t,X)
+%Creates a lorenz system with some fixed constant sigma rho beta paramters
+
+    sigma = 10;
+    rho = 28;
+    beta = 8/3;
+    
+    x = X(1);
+    y = X(2);
+    z = X(3);
+    
+    dx = sigma * (y - x);
+    dy = x * (rho - z) - y;
+    dz = x*y - beta * z;
+
+    dX = [dx; dy; dz];
+end
